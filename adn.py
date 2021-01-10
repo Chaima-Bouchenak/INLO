4c85655c77e951062965ef3eeb443166d6b65cf7
chaima <bouchenak.chuxi@gmail.com>
2021-01-10 22:02:15 +0100

    2nd commit

--- adn.py
+++ adn.py
@@ -1,22 +1,25 @@
 #Validation sequence fonction
-def is_valid_(ADN):
-    for i in range(len(ADN)):
-        if ADN[i] == "a" or ADN[i] == "t" or ADN[i] == "c" or ADN[i] == "g":
+def is_valid_(x):
+    for i in range(len(x)):
+        if x[i] == "a" or x[i] == "t" or x[i] == "c" or x[i] == "g":
             print(True)
         else:
             print(False)
 
 
 #User sequence fonction
-def get_valid_adn(prompt) :
+def get_valid_adn() :
     while True:
-        sequence = input(prompt)
+        sequence = input("Enter your ADN sequence: ")
         try:
             val = int(sequence)
             print("The sequence can containe ONLY string, please try again!")
             continue
         except ValueError:
             break
-    sequence = is_valid_(sequence)
 
-adn = get_valid_adn("Enter your ADN sequence: ")
+    sequence = is_valid_(sequence)
+    return sequence
+
+adn = get_valid_adn()
+print(adn)
